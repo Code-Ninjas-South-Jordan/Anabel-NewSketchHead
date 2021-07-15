73021440946a7d495e9e5197bd4f386cbf5ad566
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-07-14 18:01:05 -0600

    ds

--- AD-NewSketchHead/Assets/Scripts/GameController.cs
+++ AD-NewSketchHead/Assets/Scripts/GameController.cs
@@ -7,6 +7,8 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     [Header("Platform Game Object")]
     public GameObject platform;
+    [Header("Fake Platform")]
+    public GameObject fakePlat;
     float pos = 0;
     [Header("Game Over UI Canvas")]
     public GameObject gameOverCanvas;
@@ -15,6 +17,11 @@ public class GameController : MonoBehaviour
         for(int i = 0; i < 1000; i++)
         {
             SpawnPlatforms();
+        }
+        pos = 0;
+        for(int i = 0; i < 100; i++)
+        {
+            SpawnFakePlatforms();
         }
     }
     // Update is called once per frame
@@ -25,6 +32,11 @@ public class GameController : MonoBehaviour
     void SpawnPlatforms()
     {
         Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+    void SpawnFakePlatforms()
+    {
+        Instantiate(fakePlat, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
         pos += 2.5f;
     }
     public void GameOver()
